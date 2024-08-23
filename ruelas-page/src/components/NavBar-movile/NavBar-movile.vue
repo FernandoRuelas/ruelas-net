147c8e492913920918c0e2a21a1501cb868c3b1a
Fernando Rodriguez <fernandor.ruelas@gmail.com>
2024-08-23 08:25:40 -0700

    Add navegation to movile navbar

--- ruelas-page/src/components/NavBar-movile/NavBar-movile.vue
+++ ruelas-page/src/components/NavBar-movile/NavBar-movile.vue
@@ -22,19 +22,18 @@
 export default {
   name: "navBar-movile",
   methods: {
-    
     scrollTo(id) {
-    const element = document.getElementById(id);
-    const container = document.querySelector(".HomeView");
+      const element = document.getElementById(id);
+      const container = document.querySelector(".HomeView");
 
-    if (element && container) {
+      if (element && container) {
         const elementPosition = element.offsetTop;
-      
+
         const scrollPosition = elementPosition - 47; // 47 is the heigth of header
 
         container.scrollTo({ top: scrollPosition, behavior: "smooth" });
-    }
-}
+      }
+    },
   },
 };
 </script>
